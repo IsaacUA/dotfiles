38ce5f530b9d91fddae18042593c0a7f6b6093d1
IsaacAzimovUA <109369640+IsaacAzimovUA@users.noreply.github.com>
2024-01-08 10:42:21 +0200

    Add default zsh
    Adding homebrew zsh to /etc/shells

---
+++ setup_zsh.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+# TODO: Keep an eye out for a different '—no-quarantine' solution.
+# Currently, you can't-do 'brew bundle —-no-quarantine' • as an option.
+# It's currently exported in zshrc:
+# export HOMEBREW_CASK_OPTS="--no-quarantine"
+# https://github.com/Homebrew/homebrew-bundle/issues/474
+echo '\n<<<Starting ZSH setup >>>\n'
+echo '<<Enter sudo to edit /etc/shells>>'
+echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'
+echi '<<Enter user password to change login shell>>'
+chsh -s '/opt/homebrew/bin/zsh'
