32792e71437e6b93b3f533d72a3c0c9bb8be1a92
IsaacAzimovUA <109369640+IsaacAzimovUA@users.noreply.github.com>
2024-01-08 11:28:00 +0200

    FIx doubling /etc/shells path

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -6,7 +6,21 @@
 # export HOMEBREW_CASK_OPTS="--no-quarantine"
 # https://github.com/Homebrew/homebrew-bundle/issues/474
 echo '\n<<<Starting ZSH setup >>>\n'
-echo '<<Enter sudo to edit /etc/shells>>'
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' > /dev/null
-echi '<<Enter user password to change login shell>>'
-chsh -s '/opt/homebrew/bin/zsh'
+
+if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
+  echo '/opt/homebrew/bin/zsh already exists in /etc/shells'
+else
+  echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
+
+if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
+  echo '$SHELL is already /opt/homebrew/bin/zsh'
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
+
+
+
+
