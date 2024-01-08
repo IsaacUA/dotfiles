8c72efc2315b1c6fbede57bb3f3dd5ac6a6eca47
IsaacAzimovUA <109369640+IsaacAzimovUA@users.noreply.github.com>
2024-01-08 13:40:26 +0200

    Add setup_node.zsh and matching Dotbot commands install latest verion of node

---
+++ setup_node.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+# TODO: Keep an eye out for a different '—no-quarantine' solution.
+# Currently, you can't-do 'brew bundle —-no-quarantine' • as an option.
+# It's currently exported in zshrc:
+# export HOMEBREW_CASK_OPTS="--no-quarantine"
+# https://github.com/Homebrew/homebrew-bundle/issues/474
+echo "\n<<<Starting NODE setup >>>\n"
+
+if exists node; then
+    echo "NODE $(node -v) & NPM $(npm --version) are already installed"
+else
+    echo "Installing NODE & NPM with n..."
+    n latest
+fi
